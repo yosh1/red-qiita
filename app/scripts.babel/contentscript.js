68c118e122adff72236d6eace9126553d28ffdc8
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-08-12 15:39:01 +0900

    First commit

---
+++ app/scripts.babel/contentscript.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(function () {
+  $('.p-home_menuItem-active').css('background-color', '#ff0000');
+  $('.st-Header').css('background-color', '#ff0000');
+  $('.st-Header_postButton').css('background-color', '#ff0000');
+  $('.label-info').css('background-color', '#ff0000');
+  $('.it-Actions_likeCount').css('color', '#ff0000');  
+  $('.it-Actions_item-like.liked button').css('background-color', '#ff0000');
+  $('.it-Actions_item-stock button').css({'background-color':'#ff0000','border':'1px solid #ff0000'});
+  $('svg.social-icon--twitter circle, svg.social-icon--facebook circle').css('fill', '#ff0000');
+  $('.userActivityChart_stat.active, .userActivityChart_stat:hover').css('border-top', '3px solid #ff0000');
+});
